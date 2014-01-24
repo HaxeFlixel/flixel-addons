4ffe03b9875e7b91f4b5949f809197a29e466b12
SeiferTim <seifertim@gmail.com>
2014-01-24 09:46:57 -0600

    Changes to FlxAsyncLoop
    Minor changes to make it work better.
    Switched from `async` package to `util`

---
+++ flixel/addons/util/FlxAsyncLoop.hx
@@ -0,0 +1,74 @@
+package flixel.util.async;
+
+import flixel.FlxBasic;
+
+/**
+ * Special class for asynchonously performing a loop.
+ * @author Timothy Ian Hely / SeiferTim
+ */
+
+class FlxAsyncLoop extends FlxBasic
+{
+	
+	public var _started(default, null):Bool = false;
+	public var _finished(default, null):Bool = false;
+	private var _curIndex:Int = 0;
+	private var _iterationsPerUpdate:Int;
+	private var _iterations:Int;
+	private var _callback:Void->Void;
+	
+	/**
+	 * Creates an instance of the FlxAsyncLoop class, used to do a loop while still allowing update() to get called and the screen to refresh.
+	 * 
+	 * @param	Iterations				How many total times should it loop
+	 * @param	Callback				The function that should be called each loop
+	 * @param	?IterationsPerUpdate	Optional: how many loops before we allow an update() - defaults to 100.
+	 */
+	public function new(Iterations:Int, Callback:Void->Void, ?LoopObject:Dynamic = null, ?IterationsPerUpdate:Int = 100) 
+	{
+		super();
+		visible = false;
+		_object = LoopObject;
+		_iterations = Iterations;
+		_callback = Callback;
+		_iterationsPerUpdate = IterationsPerUpdate;
+	}
+	
+	
+	/**
+	 * Start the loop (if it's not already started or finished)
+	 */
+	public function start():Void
+	{
+		if (_finished || _started)
+			return;
+		_curIndex = 0;
+		_started = true;
+	}
+	
+	override public function update():Void 
+	{
+		if (!_started || _finished)
+			return;
+		
+		var startNo:Int = _curIndex;
+		for (i in startNo...Std.int(Math.min(startNo + _iterationsPerUpdate, _iterations)))
+		{
+			// call our function
+			_curIndex();
+			_curIndex++;
+		}
+		if (_curIndex >= _iterations)
+			_finished = true;
+		
+		super.update();
+	}
+	
+	
+	override public function destroy():Void 
+	{
+		_curIndex = null;
+		super.destroy();
+	}
+	
+}
