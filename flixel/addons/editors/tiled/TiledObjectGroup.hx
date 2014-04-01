c1ef24b01a499755c326df0c1c6bdac726ec3453
Samuel Batista <sambatista1@gmail.com>
2014-03-31 23:28:18 -0400

    Adding color var to TiledObjectGroups.

--- flixel/addons/editors/tiled/TiledObjectGroup.hx
+++ flixel/addons/editors/tiled/TiledObjectGroup.hx
@@ -9,9 +9,9 @@ import haxe.xml.Fast;
  */
 class TiledObjectGroup
 {
-
-    public var map:TiledMap;
+	public var map:TiledMap;
 	public var name:String;
+	public var color:Int;
 	public var opacity:Float;
 	public var visible:Bool;
 	public var properties:TiledPropertySet;
@@ -26,6 +26,14 @@ class TiledObjectGroup
 		name = Source.att.name;
 		visible = (Source.has.visible && Source.att.visible == "1") ? true : false;
 		opacity = (Source.has.opacity) ? Std.parseFloat(Source.att.opacity) : 0;
+		if (Source.has.color)
+		{
+			var hex = Source.att.color;
+			hex = "0x" + hex.substring(1, hex.length); // replace # with 0x
+			color = Std.parseInt(hex);
+		}
+		else
+			color = 0;
 		
 		// load properties
 		var node:Fast;
@@ -34,7 +42,7 @@ class TiledObjectGroup
 		{
 			properties.extend(node);
 		}
-			
+		
 		// load objects
 		for (node in Source.nodes.object)
 		{
