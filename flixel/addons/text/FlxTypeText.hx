9076a445bb4a9dd4733781c2c4d41b1af4d0725f
Gama11 <jensfischer95@gmail.com>
2014-05-10 22:46:33 +0200

    FlxTypeText: use Void->Void instead of Dynamic callbacks
    also removed setSkipKeys() and setSound(); you can set those directly

--- flixel/addons/text/FlxTypeText.hx
+++ flixel/addons/text/FlxTypeText.hx
@@ -23,132 +23,99 @@ class FlxTypeText extends FlxText
 	 * The delay between each character, in seconds.
 	 */
 	public var delay:Float = 0.05;
-	
 	/**
 	 * The delay between each character erasure, in seconds.
 	 */
 	public var eraseDelay:Float = 0.02;
-	
 	/**
 	 * Set to true to show a blinking cursor at the end of the text.
 	 */
 	public var showCursor:Bool = false;
-	
 	/**
 	 * The character to blink at the end of the text.
 	 */
 	public var cursorCharacter:String = "|";
-	
 	/**
 	 * The speed at which the cursor should blink, if shown at all.
 	 */
 	public var cursorBlinkSpeed:Float = 0.5;
-	
 	/**
 	 * Text to add at the beginning, without animating.
 	 */
 	public var prefix:String = "";
-	
 	/**
 	 * Whether or not to erase this message when it is complete.
 	 */
 	public var autoErase:Bool = false;
-	
 	/**
 	 * How long to pause after finishing the text before erasing it. Only used if autoErase is true.
 	 */
 	public var waitTime:Float = 1.0;
-	
 	/**
 	 * Whether or not to animate the text. Set to false by start() and erase().
 	 */
 	public var paused:Bool = false;
-	
 	/**
 	 * If this is set to true, this class will use typetext.wav from flixel-addons for the type sound unless you specify another.
 	 */
 	public var useDefaultSound:Bool = false;
-	
 	/**
 	 * The sound that is played when letters are added; optional.
 	 */
 	public var sound:FlxSound;
-	
 	/**
 	 * An array of keys as string values (e.g. "SPACE", "L") that will advance the text.
 	 */
-	public var skipKeys:Array<String>;
+	public var skipKeys:Array<String> = [];
+	/**
+	 * This function is called when the message is done typing.
+	 */
+	public var completeCallback:Void->Void;
+	/**
+	 * This function is called when the message is done erasing, if that is enabled.
+	 */
+	public var eraseCallback:Void->Void;
 	
 	/**
 	 * The text that will ultimately be displayed.
 	 */
 	private var _finalText:String = "";
-	
-	/**
-	 * This function is called when the message is done typing.
-	 */
-	private var _onComplete:Dynamic = null;
-	
-	/**
-	 * Optional parameters that will be passed to the _onComplete function.
-	 */
-	private var _onCompleteParams:Array<Dynamic>;
-	
-	/**
-	 * This function is called when the message is done erasing, if that is enabled.
-	 */
-	private var _onErase:Dynamic = null;
-	
-	/**
-	 * Optional parameters that will be passed to the _onErase function.
-	 */
-	private var _onEraseParams:Array<Dynamic>;
-	
 	/**
 	 * This is incremented every frame by FlxG.elapsed, and when greater than delay, adds the next letter.
 	 */
 	private var _timer:Float = 0.0;
-	
 	/**
 	 * A timer that is used while waiting between typing and erasing.
 	 */
 	private var _waitTimer:Float = 0.0;
-	
 	/**
 	 * Internal tracker for current string length, not counting the prefix.
 	 */
 	private var _length:Int = 0;
-	
 	/**
 	 * Whether or not to type the text. Set to true by start() and false by pause().
 	 */
 	private var _typing:Bool = false;
-	
 	/**
 	 * Whether or not to erase the text. Set to true by erase() and false by pause().
 	 */
 	private var _erasing:Bool = false;
-	
 	/**
 	 * Whether or not we're waiting between the type and erase phases.
 	 */
 	private var _waiting:Bool = false;
-	
 	/**
 	 * Internal tracker for cursor blink time.
 	 */
 	private var _cursorTimer:Float = 0.0;
-	
 	/**
 	 * Whether or not to add a "natural" uneven rhythm to the typing speed.
 	 */
 	private var _typingVariation:Bool = false;
-	
 	/**
 	 * How much to vary typing speed, as a percent. So, at 0.5, each letter will be "typed" up to 50% sooner or later than the delay variable is set.
 	 */
 	private var _typeVarPercent:Float = 0.5;
-	
 	/**
 	 * Helper string to reduce garbage generation.
 	 */
@@ -169,49 +136,6 @@ class FlxTypeText extends FlxText
 	{
 		super(X, Y, Width, "", Size, EmbeddedFont);
 		_finalText = Text;
-		
-		_onComplete = null;
-		_onErase = null;
-		_onCompleteParams = [];
-		_onEraseParams = [];
-		skipKeys = [];
-	}
-	
-	/**
-	 * Set a function to be called when typing the message is complete.
-	 * 
-	 * @param	Callback	The callback function.
-	 * @param	Params		Any params you want to pass to the function. Optional!
-	 */
-	public function setCompleteCallback(Callback:Dynamic, ?Params:Array<Dynamic>):Void
-	{
-		_onComplete = Callback;
-		
-		if (Params == null)
-		{
-			Params = [];
-		}
-		
-		_onCompleteParams = Params;
-	}
-	
-	/**
-	 * Set a function to be called when erasing is complete.
-	 * Make sure to set erase = true or else this will never be called!
-	 * 
-	 * @param	Callback		The callback function.
-	 * @param	Params			Any params you want to pass to the function. Optional!
-	 */
-	public function setEraseCallback(Callback:Dynamic, ?Params:Array<Dynamic>):Void
-	{
-		_onErase = Callback;
-		
-		if (Params == null)
-		{
-			Params = [];
-		}
-		
-		_onEraseParams = Params;
 	}
 	
 	/**
@@ -225,7 +149,7 @@ class FlxTypeText extends FlxText
 	 * @param	Callback		An optional callback function, to be called when the typing animation is complete.
 	 * @param 	Params			Optional parameters to pass to the callback function.
 	 */
-	public function start(?Delay:Float, ForceRestart:Bool = false, AutoErase:Bool = false, ?Sound:FlxSound, ?SkipKeys:Array<String>, ?Callback:Dynamic, ?Params:Array<Dynamic>):Void
+	public function start(?Delay:Float, ForceRestart:Bool = false, AutoErase:Bool = false, ?Sound:FlxSound, ?SkipKeys:Array<String>, ?Callback:Void->Void):Void
 	{
 		if (Delay != null)
 		{
@@ -263,12 +187,7 @@ class FlxTypeText extends FlxText
 		
 		if (Callback != null)
 		{
-			_onComplete = Callback;
-		}
-		
-		if (Params != null)
-		{
-			_onCompleteParams = Params;
+			completeCallback = Callback;
 		}
 	}
 	
@@ -282,7 +201,7 @@ class FlxTypeText extends FlxText
 	 * @param	Callback		An optional callback function, to be called when the erasing animation is complete.
 	 * @param	Params			Optional parameters to pass to the callback function.
 	 */
-	public function erase(?Delay:Float, ForceRestart:Bool = false, ?Sound:FlxSound, ?SkipKeys:Array<String>, ?Callback:Dynamic, ?Params:Array<Dynamic>):Void
+	public function erase(?Delay:Float, ForceRestart:Bool = false, ?Sound:FlxSound, ?SkipKeys:Array<String>, ?Callback:Void->Void):Void
 	{
 		_erasing = true;
 		_typing = false;
@@ -318,12 +237,7 @@ class FlxTypeText extends FlxText
 		
 		if (Callback != null)
 		{
-			_onErase = Callback;
-		}
-		
-		if (Params != null)
-		{
-			_onEraseParams = Params;
+			eraseCallback = Callback;
 		}
 	}
 	
@@ -340,26 +254,6 @@ class FlxTypeText extends FlxText
 		_erasing = false;
 		paused = false;
 		_waiting = false;
-	}
-	
-	/**
-	 * Define the keys that can be used to advance text.
-	 * 
-	 * @param	Keys	An array of keys as string values (e.g. "SPACE", "L") that will advance the text.
-	 */
-	public function setSkipKeys(Keys:Array<String>):Void
-	{
-		skipKeys = Keys;
-	}
-	
-	/**
-	 * Set a sound that will be played each time a letter is added to the text.
-	 * 
-	 * @param	Sound	A FlxSound object.
-	 */
-	public function setSound(Sound:FlxSound):Void
-	{
-		sound = Sound;
 	}
 	
 	/**
@@ -392,9 +286,9 @@ class FlxTypeText extends FlxText
 		_timer = 0;
 		_typing = false;
 		
-		if (_onComplete != null)
+		if (completeCallback != null)
 		{
-			Reflect.callMethod(null, _onComplete, _onCompleteParams);
+			completeCallback();
 		}
 		
 		if (autoErase && waitTime <= 0)
@@ -413,16 +307,15 @@ class FlxTypeText extends FlxText
 		_timer = 0;
 		_erasing = false;
 		
-		if (_onErase != null)
+		if (eraseCallback != null)
 		{
-			Reflect.callMethod(null, _onErase, _onEraseParams);
+			eraseCallback();
 		}
 	}
 	
 	override public function update():Void
 	{
 		// If the skip key was pressed, complete the animation.
-		
 		#if !FLX_NO_KEYBOARD
 		if (skipKeys != null && skipKeys.length > 0 && FlxG.keys.anyJustPressed(skipKeys))
 		{
@@ -442,7 +335,6 @@ class FlxTypeText extends FlxText
 		}
 		
 		// So long as we should be animating, increment the timer by time elapsed.
-		
 		if (!_waiting && !paused)
 		{
 			if (_length < _finalText.length && _typing)
@@ -498,11 +390,9 @@ class FlxTypeText extends FlxText
 		}
 		
 		// Update the helper string with what could potentially be the new text.
-		
 		helperString = prefix + _finalText.substr(0, _length);
 		
 		// Append the cursor if needed.
-		
 		if (showCursor)
 		{
 			_cursorTimer += FlxG.elapsed;
@@ -519,20 +409,17 @@ class FlxTypeText extends FlxText
 		}
 		
 		// If the text changed, update it.
-		
 		if (helperString != text)
 		{
 			text = helperString;
 			
 			// If we're done typing, call the onComplete() function
-			
 			if (_length >= _finalText.length && _typing && !_waiting && !_erasing)
 			{
 				onComplete();
 			}
 			
 			// If we're done erasing, call the onErased() function
-			
 			if (_length == 0 && _erasing && !_typing && !_waiting)
 			{
 				onErased();
