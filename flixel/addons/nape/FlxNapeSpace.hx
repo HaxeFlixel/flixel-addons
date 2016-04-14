6f9da7b26503d5631216e6dee4939b1406d2cb1d
Tommy Elfving <xerosugar@gmail.com>
2016-04-15 01:27:22 +0200

    put back missing files

---
+++ flixel/addons/nape/FlxNapeSpace.hx
@@ -0,0 +1,207 @@
+package flixel.addons.nape;
+
+import flash.display.BitmapData;
+import flixel.FlxBasic;
+import flixel.FlxG;
+import flixel.system.ui.FlxSystemButton;
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.phys.Material;
+import nape.shape.Polygon;
+import nape.space.Space;
+
+#if !FLX_NO_DEBUG
+import nape.util.ShapeDebug;
+
+@:bitmap("assets/images/napeDebug.png")
+class GraphicNapeDebug extends BitmapData {}
+#end
+
+/**
+ * FlxNapeSpace is a flixel plugin that integrates nape.space.Space
+ * to provide Nape physics simulation in Flixel.
+ */
+class FlxNapeSpace extends FlxBasic
+{
+	public static var space:Space;
+	
+	/**
+	 * The number of iterations used by nape in resolving errors in the velocities of objects.
+	 * This is together with collision detection the most expensive phase of a simulation update,
+	 * as well as the most important for stable results. (default 10)
+	 */
+	public static var velocityIterations:Int = 10;
+	/**
+	 * The number of iterations used by nape in resolving errors in the positions of objects.
+	 * This is far more lightweight than velocity iterations, as well as being less important
+	 * for the stability of results. (default 10)
+	 */
+	public static var positionIterations:Int = 10;
+	
+	/**
+	 * Whether or not the nape debug graphics are enabled.
+	 */
+	public static var drawDebug(default, set):Bool;
+	
+	#if !FLX_NO_DEBUG
+	/**
+	 * A useful "canvas" which can be used to draw debug information on.
+	 * To get a better idea of its use, see the official Nape demo 'SpatialQueries'
+	 * (http://napephys.com/samples.html#swf-SpatialQueries)
+	 * where this is used to draw lines emitted from Rays.
+	 * A sensible place to use this would be the state's draw() method.
+	 * Note that shapeDebug is null if drawDebug is false.
+	 */
+	public static var shapeDebug(default, null):ShapeDebug;
+	private static var drawDebugButton:FlxSystemButton;
+	#end
+	
+	/**
+	 * Needs to be called before creating any FlxNapeSprites
+	 * / FlxNapeTilemaps to initialize the space.
+	 */
+	public static function init():Void
+	{
+		FlxG.plugins.add(new FlxNapeSpace());
+		
+		if (space == null)
+		{
+			space = new Space(new Vec2());
+		}
+		
+		FlxG.signals.stateSwitched.add(onStateSwitch);
+		
+		#if !FLX_NO_DEBUG
+		// Add a button to toggle Nape debug shapes to the debugger
+		drawDebugButton = FlxG.debugger.addButton(RIGHT, new GraphicNapeDebug(0, 0), function()
+		{
+			drawDebug = !drawDebug;
+		}, true, true);
+		drawDebug = false;
+		#end
+	}
+	
+	/**
+	 * Creates simple walls around the game area - useful for prototying.
+	 *
+	 * @param   minX        The smallest X value of your level (usually 0).
+	 * @param   minY        The smallest Y value of your level (usually 0).
+	 * @param   maxX        The largest X value of your level - 0 means FlxG.width (usually the level width).
+	 * @param   maxY        The largest Y value of your level - 0 means FlxG.height (usually the level height).
+	 * @param   thickness   How thick the walls are. 10 by default.
+	 * @param   material    The Material to use for the physics body of the walls.
+	 */
+	public static function createWalls(minX:Float = 0, minY:Float = 0, maxX:Float = 0, maxY:Float = 0, thickness:Float = 10, ?material:Material):Body
+	{
+		if (maxX == 0)
+		{
+			maxX = FlxG.width;
+		}
+		
+		if (maxY == 0)
+		{
+			maxY = FlxG.height;
+		}
+		
+		if (material == null)
+		{
+			material = new Material(0.4, 0.2, 0.38, 0.7);
+		}
+		
+		var walls:Body = new Body(BodyType.STATIC);
+		
+		// Left wall
+		walls.shapes.add(new Polygon(Polygon.rect(minX - thickness, minY, thickness, maxY + Math.abs(minY))));
+		// Right wall
+		walls.shapes.add(new Polygon(Polygon.rect(maxX, minY, thickness, maxY + Math.abs(minY))));
+		// Upper wall
+		walls.shapes.add(new Polygon(Polygon.rect(minX, minY - thickness, maxX + Math.abs(minX), thickness)));
+		// Bottom wall
+		walls.shapes.add(new Polygon(Polygon.rect(minX, maxY, maxX + Math.abs(minX), thickness)));
+
+		walls.space = space;
+		walls.setShapeMaterials(material);
+		
+		return walls;
+	}
+	
+	private static function set_drawDebug(drawDebug:Bool):Bool
+	{
+		#if !FLX_NO_DEBUG
+		if (drawDebugButton != null)
+			drawDebugButton.toggled = !drawDebug;
+		
+		if (drawDebug)
+		{
+			if (shapeDebug == null)
+			{
+				shapeDebug = new ShapeDebug(FlxG.width, FlxG.height);
+				shapeDebug.drawConstraints = true;
+				shapeDebug.display.scrollRect = null;
+				shapeDebug.thickness = 1;
+				FlxG.addChildBelowMouse(shapeDebug.display);
+			}
+		}
+		else if (shapeDebug != null)
+		{
+			FlxG.removeChild(shapeDebug.display);
+			shapeDebug = null;
+		}
+		#end
+		
+		return FlxNapeSpace.drawDebug = drawDebug;
+	}
+	
+	private static function onStateSwitch():Void
+	{
+		if (space != null)
+		{
+			space.clear();
+			space = null; // resets atributes like gravity.
+		}
+		
+		#if !FLX_NO_DEBUG
+		drawDebug = false;
+		
+		if (drawDebugButton != null)
+		{
+			FlxG.debugger.removeButton(drawDebugButton);
+			drawDebugButton = null;
+		}
+		#end
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		if (space != null && elapsed > 0)
+		{
+			space.step(elapsed, velocityIterations, positionIterations);
+		}
+	}
+	
+	/**
+	 * Draws debug graphics.
+	 */
+	override public function draw():Void
+	{
+		#if !FLX_NO_DEBUG
+		if (shapeDebug == null || space == null)
+		{
+			return;
+		}
+		
+		shapeDebug.clear();
+		shapeDebug.draw(space);
+		
+		var zoom = FlxG.camera.zoom;
+		var sprite = shapeDebug.display;
+		
+		sprite.scaleX = zoom;
+		sprite.scaleY = zoom;
+		
+		sprite.x = -FlxG.camera.scroll.x * zoom;
+		sprite.y = -FlxG.camera.scroll.y * zoom;
+		#end
+	}
+}
---
+++ flixel/addons/nape/FlxNapeSprite.hx
@@ -0,0 +1,308 @@
+package flixel.addons.nape;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets;
+import flixel.math.FlxAngle;
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.phys.Material;
+import nape.shape.Circle;
+import nape.shape.Polygon;
+import nape.space.Space;
+
+/**
+ * FlxNapeSprite consists of a FlxSprite with a physics body.
+ * During the simulation, the sprite follows the physics body position and rotation.
+ * 
+ * By default, a rectangular physics body is created upon construction in createRectangularBody().
+ * 
+ * @author TiagoLr ( ~~~ProG4mr~~~ )
+ */
+class FlxNapeSprite extends FlxSprite
+{
+	/**
+	 * The physics body associated with this sprite. 
+	 */
+	public var body:Body;
+	
+	/**
+	 * Enables/disables this sprite's physics body in simulations
+	 * by adding/removing it from the space.
+	 */
+	public var physicsEnabled(default, set):Bool = false;
+	
+	/**
+	 * Internal var to update body.velocity.x and body.velocity.y. 
+	 * Default is 1, which menas no drag.
+	 */
+	private var _linearDrag:Float = 1;
+	/**
+	 * Internal var to update body.angularVel
+	 * Default is 1, which menas no drag.
+	 */
+	private var _angularDrag:Float = 1;
+
+	/**
+	 * Creates a FlxNapeSprite with an optional physics body.
+	 * At each step, the physics are updated, and so is the position and rotation of the sprite 
+	 * to match the bodys position and rotation values.
+	 * By default, a physics body with a rectangular shape will be created for the sprite's graphic.
+	 * You can override this functionality and add a premade body of your own (see addPremadeBody()).
+	 * 
+	 * @param	X						The initial x position of the sprite.
+	 * @param	Y						The initial y position of the sprite.
+	 * @param	SimpleGraphic 			The graphic you want to display (OPTIONAL - for simple stuff only, do NOT use for animated images!).
+	 * @param	CreateRectangularBody	Whether to create a rectangular body for this sprite (use false if you want to add a custom body).
+	 * @param	EnablePhysics			Whether to enable physics simulation for the body (by adding it to the space)
+	 */
+	public function new(X:Float = 0, Y:Float = 0, ?SimpleGraphic:FlxGraphicAsset, CreateRectangularBody:Bool = true, EnablePhysics:Bool = true) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		if (CreateRectangularBody)
+		{
+			createRectangularBody();
+		}
+		physicsEnabled = EnablePhysics;
+	}
+
+	/**
+	 * WARNING: This will remove this sprite entirely. Use kill() if you 
+	 * want to disable it temporarily only and reset() it later to revive it.
+	 * Override this function to null out variables or manually call
+	 * destroy() on class members if necessary.
+	 * Don't forget to call super.destroy()!
+	 */
+	override public function destroy():Void 
+	{
+		destroyPhysObjects();
+		
+		super.destroy();
+	}
+
+	/**
+	 * Override core physics velocity etc
+	 */
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+		if (body != null && moves)
+		{
+			updatePhysObjects();
+		}
+	}
+
+	/**
+	 * Handy function for "killing" game objects.
+	 * Default behavior is to flag them as nonexistent AND dead.
+	 */
+	override public function kill():Void
+	{
+		super.kill();
+		
+		if (body != null)
+		{
+			body.space = null;
+		}
+	}
+
+	/**
+	 * Handy function for bringing game objects "back to life". Just sets alive and exists back to true.
+	 * In practice, this function is most often called by FlxObject.reset().
+	 */
+	override public function revive():Void
+	{
+		super.revive();
+		
+		if (body != null)
+		{
+			body.space = FlxNapeSpace.space;
+		}
+	}
+	
+	/**
+	 * Makes it easier to add a physics body of your own to this sprite by setting its position,
+	 * space and material for you.
+	 * 
+	 * @param	NewBody 	The new physics body replacing the old one.
+	 */
+	public function addPremadeBody(NewBody:Body):Void
+	{
+		if (body != null) 
+		{
+			destroyPhysObjects();
+		}
+		
+		NewBody.position.x = x;
+		NewBody.position.y = y;
+		setBody(NewBody);
+		setBodyMaterial();
+	}
+	
+	/**
+	 * Creates a circular physics body for this sprite.
+	 * 
+	 * @param	Radius	The radius of the circle-shaped body - 16 by default
+	 * @param 	_Type	The BodyType of the physics body. Optional, DYNAMIC by default.
+	 */
+	public function createCircularBody(Radius:Float = 16, ?_Type:BodyType):Void
+	{
+		if (body != null) 
+		{
+			destroyPhysObjects();
+		}
+		
+		centerOffsets(false);
+		setBody(new Body(_Type != null ? _Type : BodyType.DYNAMIC, Vec2.weak(x, y)));
+		body.shapes.add(new Circle(Radius));
+		
+		setBodyMaterial();
+	}
+	
+	/**
+	 * Default method to create the physics body used by this sprite in shape of a rectangle.
+	 * Override this method to create your own physics body!
+	 * Call this method after calling makeGraphics() or loadGraphic() to update the body size.
+	 * 
+	 * @param	Width	The width of the rectangle. Uses frameWidth if <= 0.
+	 * @param	Height	The height of the rectangle. Uses frameHeight if <= 0.
+	 * @param	_Type	The BodyType of the physics body. Optional, DYNAMIC by default.
+	 */
+	public function createRectangularBody(Width:Float = 0, Height:Float = 0, ?_Type:BodyType):Void
+	{
+		if (body != null) 
+		{
+			destroyPhysObjects();
+		}
+		
+		if (Width <= 0)
+		{
+			Width = frameWidth;
+		}
+		if (Height <= 0)
+		{
+			Height = frameHeight;
+		}
+		
+		centerOffsets(false);
+		setBody(new Body(_Type != null ? _Type : BodyType.DYNAMIC, Vec2.weak(x, y)));
+		body.shapes.add(new Polygon(Polygon.box(Width, Height)));
+		
+		setBodyMaterial();
+	}
+	
+	/**
+	 * Shortcut method to set/change the physics body material.
+	 * 
+	 * @param	Elasticity			Elasticity of material.		
+	 * @param	DynamicFriction		Coeffecient of dynamic friction for material.
+	 * @param	StaticFriction		Coeffecient of static friction for material.
+	 * @param	Density				Density of this Material.
+	 * @param	RotationFriction	Coeffecient of rolling friction for circle interactions.
+	 */
+	public function setBodyMaterial(Elasticity:Float = 1, DynamicFriction:Float = 0.2, StaticFriction:Float = 0.4, Density:Float = 1, RotationFriction:Float = 0.001):Void
+	{
+		if (body == null) 
+			return;
+		
+		body.setShapeMaterials(new Material(Elasticity, DynamicFriction, StaticFriction, Density, RotationFriction));
+	}
+	
+	/**
+	 * Destroys the physics main body.
+	 */
+	public function destroyPhysObjects():Void
+	{
+		if (body != null) 
+		{
+			if (FlxNapeSpace.space != null)
+				FlxNapeSpace.space.bodies.remove(body);
+			body = null;
+		}
+	}
+
+	/**
+	 * Nape requires fluid spaces to add empty space linear drag and angular drag.
+	 * This provides a simple drag alternative. 
+	 * Set any values to linearDrag or angularDrag to activate this feature for this object.
+	 * 
+	 * @param	LinearDrag		Typical value 0.96 (1 = no drag).
+	 * @param	AngularDrag		Typical value 0.96 (1 = no drag);
+	 */
+	public inline function setDrag(LinearDrag:Float = 1, AngularDrag:Float = 1):Void
+	{
+		_linearDrag	= LinearDrag;
+		_angularDrag = AngularDrag;
+	}
+	
+	#if !FLX_NO_DEBUG
+	/**
+	 * Hide debug outline on physics sprites if the physics debug shapes are turned on
+	 */	
+	override public function drawDebug():Void
+	{
+		if (!FlxNapeSpace.drawDebug)
+		{
+			super.drawDebug();
+		}
+	}
+	#end
+	
+	private function setBody(body:Body):Void
+	{
+		this.body = body;
+		set_physicsEnabled(physicsEnabled);
+	}
+	
+	/**
+	 * Updates physics FlxSprite graphics to follow this sprite physics object, called at the end of update().
+	 * Things that are updated: Position, angle, angular and linear drag.
+	 */	
+	private function updatePhysObjects():Void 
+	{
+		updatePosition();
+		
+		if (body.allowRotation)
+		{
+			angle = body.rotation * FlxAngle.TO_DEG;
+		}
+		
+		// Applies custom physics drag.
+		if (_linearDrag < 1 || _angularDrag < 1) 
+		{
+			body.angularVel *= _angularDrag;
+			body.velocity.x *= _linearDrag;
+			body.velocity.y *= _linearDrag;
+		}
+	}
+	
+	private function updatePosition():Void
+	{
+		x = body.position.x - origin.x;
+		y = body.position.y - origin.y;
+	}
+	
+	private inline function set_physicsEnabled(Value:Bool):Bool
+	{
+		if (body != null)
+			body.space = Value ? FlxNapeSpace.space : null;
+		return physicsEnabled = Value;
+	}
+	
+	/**
+	 * Helper function to set the coordinates of this object.
+	 * Handy since it only requires one line of code.
+	 *
+	 * @param	X	The new x position
+	 * @param	Y	The new y position
+	 */
+	override public function setPosition(X:Float = 0, Y:Float = 0):Void
+	{
+		body.position.x = X;
+		body.position.y = Y;
+		
+		updatePosition();
+	}
+}
