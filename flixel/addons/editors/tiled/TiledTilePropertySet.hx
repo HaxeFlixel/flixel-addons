4c9bedc9346f715b22846aeae497f50d7717507a
Ben Morris <ben@bendmorris.com>
2016-09-15 09:29:18 -0700

    Parse tile animations in TiledMap.

---
+++ flixel/addons/editors/tiled/TiledTilePropertySet.hx
@@ -0,0 +1,25 @@
+package flixel.addons.editors.tiled;
+
+
+typedef TileAnimationData = {
+	var tileID:Int;
+	var duration:Float;
+}
+
+class TiledTilePropertySet extends TiledPropertySet
+{
+	public var tileID:Int;
+	public var animationFrames:Array<TileAnimationData>;
+
+	public function new(tileID:Int)
+	{
+		super();
+		this.tileID = tileID;
+		animationFrames = new Array();
+	}
+
+	public function addAnimationFrame(tileID:Int, duration:Float):Void
+	{
+		animationFrames.push({tileID:tileID, duration:duration});
+	}
+}
