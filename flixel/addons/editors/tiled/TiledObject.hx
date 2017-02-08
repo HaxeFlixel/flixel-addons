HaxeFlixel/flixel-addons

commit 9d9f1b960f1e954326f5e7250ea7c1b05c14dc6e

TiledObject: fix gid handling with flipped objects, closes #287 (#288)

flixel/addons/editors/tiled/TiledObject.hx
@@ -1,5 +1,6 @@
 package flixel.addons.editors.tiled;
 
+import haxe.Int64;
 import haxe.xml.Fast;
 import flixel.math.FlxPoint;
 
@@ -12,7 +13,7 @@ class TiledObject
 	/**
 	 * Use these to determine whether a sprite should be flipped, for example:
 	 * 
-	 * var flipped:Bool = (oject.gid & TiledObject.FLIPPED_HORIZONTALLY_FLAG) > 0;
+	 * var flipped:Bool = (object.gid & TiledObject.FLIPPED_HORIZONTALLY_FLAG) > 0;
 	 * sprite.facing = flipped ? FlxObject.LEFT : FlxObject.RIGHT;
 	 */
 	public static inline var FLIPPED_VERTICALLY_FLAG = 0x40000000;
@@ -59,11 +60,11 @@ class TiledObject
 	/**
 	 * Whether the object is flipped horizontally.
 	 */
-	public var flippedHorizontally(get, null):Bool;
+	public var flippedHorizontally(default, null):Bool;
 	/**
 	 * Whether the object is flipped vertically.
 	 */
-	public var flippedVertically(get, null):Bool;
+	public var flippedVertically(default, null):Bool;
 	/**
 	 * An array with points if the object is a POLYGON or POLYLINE
 	 */
@@ -84,14 +85,20 @@ class TiledObject
 		// By default let's it be a rectangle object
 		objectType = RECTANGLE;
 		
-		// resolve inheritence
+		// resolve inheritance
 		shared = null;
 		gid = -1;
 		
 		// object with tile association?
-		if (source.has.gid && source.att.gid.length != 0) 
+		if (source.has.gid && source.att.gid.length != 0)
 		{
-			gid = Std.parseInt(source.att.gid);
+			var gid64 = Int64.parseString(source.att.gid);
+			
+			flippedHorizontally = (gid64 & FLIPPED_HORIZONTALLY_FLAG) < 0;
+			flippedVertically = (gid64 & FLIPPED_VERTICALLY_FLAG) > 0;
+
+			gid64 &= ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG);
+			gid = gid64.low;
 			
 			for (set in layer.map.tilesets)
 			{
@@ -142,15 +149,5 @@ class TiledObject
 			pair = p.split(",");
 			points.push(FlxPoint.get(Std.parseFloat(pair[0]), Std.parseFloat(pair[1])));
 		}
-	}
-
-	private inline function get_flippedHorizontally():Bool
-	{
-		return (gid & FLIPPED_HORIZONTALLY_FLAG) > 0;
-	}
-
-	private inline function get_flippedVertically():Bool
-	{
-		return (gid & FLIPPED_VERTICALLY_FLAG) > 0;
 	}
 }

commit 923eb81cdced3b8d9f8d4181a8ad92b45fc35f9f

TiledObject: fix horizontal flipping (#289)
Issue introduced in #288.

flixel/addons/editors/tiled/TiledObject.hx
@@ -94,9 +94,8 @@ class TiledObject
 		{
 			var gid64 = Int64.parseString(source.att.gid);
 			
-			flippedHorizontally = (gid64 & FLIPPED_HORIZONTALLY_FLAG) < 0;
+			flippedHorizontally = (gid64 & FLIPPED_HORIZONTALLY_FLAG) > 0;
 			flippedVertically = (gid64 & FLIPPED_VERTICALLY_FLAG) > 0;
-
 			gid64 &= ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG);
 			gid = gid64.low;
 			