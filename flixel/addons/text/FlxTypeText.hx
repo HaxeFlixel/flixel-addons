c9fdd67c057b23c877b0e5cd3a7960c6a880f2b2
Gama11 <jensfischer95@gmail.com>
2017-03-12 19:29:20 +0100

    FlxTypeText: remove old bitfive checks
    openfl-bitfive hasn't been working for a while now.

--- flixel/addons/text/FlxTypeText.hx
+++ flixel/addons/text/FlxTypeText.hx
@@ -7,8 +7,6 @@ import flixel.system.FlxAssets;
 import flixel.text.FlxText;
 import flixel.system.FlxSound;
 import flixel.math.FlxRandom;
-
-#if !bitfive
 import flash.media.Sound;
 
 #if !flash
@@ -18,7 +16,6 @@ class TypeSound extends Sound { }
 // Flash uses a WAV instead of MP3 because the sound is so short that MP3's encoding mutes most of it
 @:sound("assets/sounds/type.wav")
 class TypeSound extends Sound { }
-#end
 #end
 
 /**
@@ -69,7 +66,7 @@ class FlxTypeText extends FlxText
 	 */
 	public var sounds:Array<FlxSound>;
 	/**
-	 * Whether or not to use the default typing sound. Not available for openfl-bitfive.
+	 * Whether or not to use the default typing sound.
 	 */
 	public var useDefaultSound:Bool = false;
 	/**
@@ -190,12 +187,10 @@ class FlxTypeText extends FlxText
 
 		insertBreakLines();
 		
-		#if !bitfive
 		if (useDefaultSound)
 		{
 			loadDefaultSound();
 		}
-		#end
 	}
 	
 	/**
@@ -268,12 +263,10 @@ class FlxTypeText extends FlxText
 		
 		eraseCallback = Callback;
 		
-		#if !bitfive
 		if (useDefaultSound)
 		{
 			loadDefaultSound();
 		}
-		#end
 	}
 	
 	/**
@@ -423,9 +416,7 @@ class FlxTypeText extends FlxText
 				}
 				else if (useDefaultSound)
 				{
-					#if !bitfive
 					_sound.play(true);
-					#end
 				}
 			}
 		}
@@ -490,7 +481,6 @@ class FlxTypeText extends FlxText
 		}
 	}
 	
-	#if !bitfive
 	private function loadDefaultSound():Void
 	{
 		#if FLX_SOUND_SYSTEM
@@ -500,5 +490,4 @@ class FlxTypeText extends FlxText
 		_sound.loadEmbedded(new TypeSound());
 		#end
 	}
-	#end
 }
