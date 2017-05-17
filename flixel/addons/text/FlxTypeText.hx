HaxeFlixel/flixel-addons

commit b62c7191f6a9f3400174105eb7e1190d031281e7

FlxTypeText: stop sounds when typing is complete (#295)

flixel/addons/text/FlxTypeText.hx
@@ -312,6 +312,18 @@ class FlxTypeText extends FlxText
 		_timer = 0;
 		_typing = false;
 		
+		if (useDefaultSound)
+		{
+			_sound.stop();
+		}
+		else
+		{
+			for (sound in sounds)
+			{
+				sound.stop();
+			}
+		}
+		
 		if (completeCallback != null)
 		{
 			completeCallback();

commit cbea22e04928b20f207af8c76524b675c37c1b88

FlxTypeText: fix a crash caused by sounds being null
Regression introduced by #295.

flixel/addons/text/FlxTypeText.hx
@@ -317,7 +317,7 @@ class FlxTypeText extends FlxText
 		{
 			_sound.stop();
 		}
-		else
+		else if (sounds != null)
 		{
 			for (sound in sounds)
 			{