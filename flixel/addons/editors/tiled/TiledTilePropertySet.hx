0373e36e0853f40d6898648fedfff652be27906c
Elliot <elliocheerio@users.noreply.github.com>
2018-12-08 23:08:18 +0000

    Add Support for Tiled Collision Editor, closes #316 (#327)

--- flixel/addons/editors/tiled/TiledTilePropertySet.hx
+++ flixel/addons/editors/tiled/TiledTilePropertySet.hx
@@ -1,5 +1,7 @@
 package flixel.addons.editors.tiled;
 
+import flixel.addons.editors.tiled.TiledObject;
+
 /** @since 2.2.0 */
 typedef TileAnimationData = {
 	var tileID:Int;
@@ -11,16 +13,23 @@ class TiledTilePropertySet extends TiledPropertySet
 {
 	public var tileID:Int;
 	public var animationFrames:Array<TileAnimationData>;
+	public var tileObjects:Array<TiledObject>;
 
 	public function new(tileID:Int)
 	{
 		super();
 		this.tileID = tileID;
 		animationFrames = new Array();
+		tileObjects = new Array();
 	}
 
 	public function addAnimationFrame(tileID:Int, duration:Float):Void
 	{
 		animationFrames.push({ tileID: tileID, duration: duration });
+	}
+
+	public function addTileObject(tileObject:TiledObject):Void
+	{
+		tileObjects.push(tileObject);
 	}
 }
