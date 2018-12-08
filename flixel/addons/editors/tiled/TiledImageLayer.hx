9a87f7cef0b7526db80e44e25815f0d412f264e9
ElliotRC <elliotrc129@gmail.com>
2018-12-08 14:23:01 -0500

    TiledImageLayer: fix image coordinates for newer versions of Tiled

--- flixel/addons/editors/tiled/TiledImageLayer.hx
+++ flixel/addons/editors/tiled/TiledImageLayer.hx
@@ -9,15 +9,11 @@ import haxe.xml.Fast as Access;
 class TiledImageLayer extends TiledLayer
 {
 	public var imagePath:String;
-	public var x:Int;
-	public var y:Int;
 
 	public function new(source:Access, parent:TiledMap)
 	{
 		super(source, parent);
 		type = IMAGE;
 		imagePath = source.node.image.att.source;
-		x = (source.has.x) ? Std.parseInt(source.att.x) : 0;
-		y = (source.has.y) ? Std.parseInt(source.att.y) : 0;
 	}
 }
