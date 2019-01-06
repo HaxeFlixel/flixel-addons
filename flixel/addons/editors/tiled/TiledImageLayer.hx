e0f1a853aeca7cd6d03a130e0c9e11a6adfc7e8c
ElliotRC <elliotrc129@gmail.com>
2019-01-06 14:11:09 -0500

    TiledImageLayer: add comments for offsets for newer versions of Tiled

--- flixel/addons/editors/tiled/TiledImageLayer.hx
+++ flixel/addons/editors/tiled/TiledImageLayer.hx
@@ -9,11 +9,18 @@ import haxe.xml.Fast as Access;
 class TiledImageLayer extends TiledLayer
 {
 	public var imagePath:String;
+	/** Tiled version >= 0.15 uses offsetX */
+	public var x:Int;
+	/** Tiled version >= 0.15 uses offsetY */
+	public var y:Int;
 
 	public function new(source:Access, parent:TiledMap)
 	{
 		super(source, parent);
 		type = IMAGE;
 		imagePath = source.node.image.att.source;
+
+		x = (source.has.x) ? Std.parseInt(source.att.x) : 0;
+		y = (source.has.y) ? Std.parseInt(source.att.y) : 0;
 	}
 }
