519879ab54f03134964bf0c6f2f9bab266f6b5b9
Sword352 <99792114+Sword352@users.noreply.github.com>
2023-06-25 09:56:43 +0200

    Fix Typo

--- flixel/addons/display/FlxBackdrop.hx
+++ flixel/addons/display/FlxBackdrop.hx
@@ -68,7 +68,7 @@ class FlxBackdrop extends FlxSprite
 	 * Creates an instance of the FlxBackdrop class, used to create infinitely scrolling backgrounds.
 	 *
 	 * @param   graphic     The image you want to use for the backdrop.
-	 * @param   repeatAxes  If the backdrop should repeat on the X axis.
+	 * @param   repeatAxes  The axis where the backdrop should repeat to.
 	 * @param   spacingX    Amount of spacing between tiles on the X axis
 	 * @param   spacingY    Amount of spacing between tiles on the Y axis
 	 */
