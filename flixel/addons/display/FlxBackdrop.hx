465ecb2ddf15efd6ff132e175b115770b8e86a64
George Kurelic <Gkurelic@gmail.com>
2023-09-26 12:54:49 -0500

    Update flixel/addons/display/FlxBackdrop.hx

--- flixel/addons/display/FlxBackdrop.hx
+++ flixel/addons/display/FlxBackdrop.hx
@@ -72,7 +72,7 @@ class FlxBackdrop extends FlxSprite
 	 * @param   spacingX    Amount of spacing between tiles on the X axis
 	 * @param   spacingY    Amount of spacing between tiles on the Y axis
 	 */
-	public function new(?graphic:FlxGraphicAsset, repeatAxes:FlxAxes = XY, spacingX:Float = 0, spacingY:Float = 0)
+	public function new(?graphic:FlxGraphicAsset, repeatAxes = XY, spacingX = 0.0, spacingY = 0.0)
 	{
 		super(0, 0, graphic);
 		
