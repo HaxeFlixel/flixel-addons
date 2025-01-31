c2b8e68904eda35183dde4f880752fd86274435f
George FunBook <gkurelic@gmail.com>
2025-01-31 10:05:23 -0600

    fix dox

--- flixel/addons/display/FlxRadialGauge.hx
+++ flixel/addons/display/FlxRadialGauge.hx
@@ -150,6 +150,6 @@ class FlxRadialWipeShader extends flixel.system.FlxAssets.FlxShader
 		end = 270;
 	}
 }
-#elseif FLX_NO_COVERAGE_TEST
+#elseif (FLX_NO_COVERAGE_TEST && !dox)
 #error "FlxRadialGauge is not supported on flash targets"
 #end
